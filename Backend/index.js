b8057c64e5fe1a5f89883cc43737eb64029b1007
Anupam <anupam.r2301@gmail.com>
2025-03-24 12:09:31 +0530

    fixed .env issue

--- Backend/index.js
+++ Backend/index.js
@@ -8,11 +8,11 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-mongoose.connect("mongodb+srv://Anupam:Anu23@clusterfirst.wyae5.mongodb.net/?retryWrites=true&w=majority&appName=ClusterFirst").then(() => {
-  console.log("Mongodb Connected.")
-}).catch((err) => {
-  console.log(err)
-})
+require('dotenv').config();
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
 
 app.post("/Login", (req, res) => {
   const { email, password } = req.body;
