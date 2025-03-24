8e557d679be56682aa74a2e4af4a04d03f2dea60
Anupam <anupam.r2301@gmail.com>
2025-03-24 12:14:43 +0530

    fixed the .env issue

--- Backend/index.js
+++ Backend/index.js
@@ -10,7 +10,7 @@ app.use(cors())
 
 require('dotenv').config();
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log("MongoDB Connected"))
+  .then(() => console.log("MongoDB is connected."))
   .catch(err => console.log(err));
 
 
